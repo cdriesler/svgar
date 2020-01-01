207cfbdb3fb7907403c1408059d34aa461cfd69e
chuck driesler <cdriesler.iv@gmail.com>
2020-01-01 16:01:39 -0500

    add tests for project method

--- src/rs/lib.rs
+++ src/rs/lib.rs
@@ -14,6 +14,16 @@ pub struct Point3d {
     pub z: f64
 }
 
+impl Point3d {
+    pub fn new(x: f64, y: f64, z: f64) -> Point3d {
+        return Point3d { x: x, y: y, z: z }
+    }
+
+    pub fn equals(self: &Self, point: &Point3d) -> bool {
+        return (self.x == point.x) && (self.y == point.y) && (self.z == point.z);
+    }
+}
+
 #[wasm_bindgen]
 pub fn greet(name: &str) -> std::string::String {
     return format!("Hello, {}!", name);
@@ -25,6 +35,33 @@ pub fn why(x: &str) -> f64 {
     return my_int;
 }
 
+/// Returns a point {x, y, z} projected onto a given plane.
+///
+/// # Arguments
+///
+/// * `x` - x coordinate of point to project
+/// * `y` - y coordinate of point to project
+/// * `z` - z coordinate of point to project
+/// 
+/// * `a` - x component of plane normal
+/// * `b` - y component of plane normal
+/// * `c` - z component of plane normal
+/// 
+/// * `d` - x coordinate of a point on the plane
+/// * `e` - y coordinate of a point on the plane
+/// * `f` - z coordinate of a point on the plane
+/// 
+/// # Returns
+/// 
+/// * Point3d { x: f64, y: f64, z: f64 }
+/// 
+/// Note: The returned coordinates are in world coordinate space.
+///
+/// # Example
+///
+/// ```
+/// Nothing yet
+/// ```
 #[wasm_bindgen]
 pub fn project(x: f64, y: f64, z: f64, a: f64, b: f64, c: f64, d: f64, e: f64, f: f64) -> Point3d {
     Point3d { x: x + y + z, y: 2.0, z: 3.0 }
@@ -32,16 +69,33 @@ pub fn project(x: f64, y: f64, z: f64, a: f64, b: f64, c: f64, d: f64, e: f64, f
 
 #[cfg(test)]
 mod tests {
+    
     #[test]
     fn it_works() {
         use greet;
+
         assert_eq!("Hello, t!", greet("t"));
     }
 
     #[test]
-    fn project() {
+    fn test_project() {
         use project;
-        let pt = project(1.0, 2.0, 3.0, 1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
-        assert_eq!(pt.x, 1.0);
+        use Point3d;
+
+        let pt = project(5.0, 4.0, 3.0, 0.0, 0.0, 1.0, 3.0, 2.0, 1.0);
+
+        let target = Point3d::new(5.0, 4.0, 1.0);
+
+        assert!(pt.equals(&target));
+    }
+
+    #[test]
+    fn test_point3d_equals() {
+        use Point3d;
+
+        let a = Point3d::new(0.0, 0.0, 0.0);
+        let b = Point3d::new(0.0, 0.0, 0.0);
+
+        assert!(a.equals(&b));
     }
 }
