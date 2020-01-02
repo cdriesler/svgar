1919e4526922e6ce4aa14bec024e442c5b145213
chuck driesler <cdriesler.iv@gmail.com>
2020-01-02 00:44:01 -0500

    implement vector cross product

--- src/rs/lib.rs
+++ src/rs/lib.rs
@@ -28,6 +28,14 @@ impl Point3d {
         return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
     }
 
+    pub fn cross(a: &Point3d, b: &Point3d) -> Point3d {
+        let x = (a.y * b.z) - (a.z * b.y);
+        let y = (a.z * b.x) - (a.x * b.z);
+        let z = (a.x * b.y) - (a.y * b.x);
+
+        return Point3d{ x: x, y: y, z: z };
+    }
+
     pub fn scale(vec: &Point3d, factor: f64) -> Point3d {
         let x = vec.x * factor;
         let y = vec.y * factor;
@@ -50,7 +58,7 @@ pub fn greet(name: &str) -> std::string::String {
     return format!("Hello, {}!", name);
 }
 
-/// Returns a point {x, y, z} projected onto a given plane.
+/// Returns a point { x, y, z } projected onto a given plane.
 ///
 /// # Arguments
 ///
@@ -92,6 +100,29 @@ pub fn project(x: f64, y: f64, z: f64, a: f64, b: f64, c: f64, d: f64, e: f64, f
     let projected_point = Point3d::add(&point, &transform);
 
     return projected_point;
+}
+
+/// Returns a point { x, y, z } projected onto a 3D plane in that plane's 2D coordinate space
+/// 
+/// # Arguments
+/// 
+/// * `x` - x coordinate of point to project
+/// * `y` - y coordinate of point to project
+/// * `z` - z coordinate of point to project
+/// 
+/// * `a` - x component of plane normal
+/// * `b` - y component of plane normal
+/// * `c` - z component of plane normal
+/// 
+/// * `d` - x coordinate of a point on the plane
+/// * `e` - y coordinate of a point on the plane
+/// * `f` - z coordinate of a point on the plane
+/// 
+/// * `rotation` - 
+/// 
+#[wasm_bindgen]
+pub fn project_and_remap(x: f64, y: f64, z: f64, a: f64, b: f64, c: f64, d: f64, e: f64, f: f64, rotation: f64) -> Point3d {
+    Point3d { x: 0.0, y: 0.0, z: 0.0 }
 }
 
 #[cfg(test)]
@@ -205,5 +236,20 @@ mod tests {
         let res = Point3d::scale(&a, 1.0);
 
         assert_eq!(res.x, 1.0);
+    }
+
+    // Point3d::cross() tests
+
+    #[test]
+    fn random_vectors() {
+        use Point3d;
+
+        let a = Point3d::new(2.0, 3.0, 4.0);
+        let b = Point3d::new(5.0, 6.0, 7.0);
+
+        let res = Point3d::cross(&a, &b);
+        let target = Point3d::new(-3.0, 6.0, -3.0);
+
+        assert!(res.equals(&target));
     }
 }
