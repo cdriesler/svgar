50ed8eccf8dcfb378846b94e3805cd537f88a5a0
chuck driesler <cdriesler.iv@gmail.com>
2020-02-15 22:00:28 -0500

    test ts + rs on github actions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import SomeClass from './models/SomeClass';
-import Cream from './services/CreamService';
-import RhinoService from './services/RhinoService';
 import SvgarCube from './models/SvgarCube';
 import SvgarElement from './models/SvgarElement';
 import SvgarMaterial from './models/SvgarMaterial';
