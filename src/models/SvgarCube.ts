5051183fd0873b461932763d9fffcecce23298e0
chuck driesler <cdriesler.iv@gmail.com>
2020-05-17 00:55:36 -0400

    temporarily remove rhino3dm

--- src/models/SvgarCube.ts
+++ src/models/SvgarCube.ts
@@ -35,21 +35,23 @@ export default class SvgarCube {
         this.creamModule = await import('./../wasm/cream');
         this.camera = new Camera(this.creamModule);
 
-        this.elements = new SvgarElementsContext(this.creamModule, {});
+        // this.elements = new SvgarElementsContext(this.creamModule, {});
 
-        try {
-        await rhino3dm().then(rhino => {
-            this.rhinoModule = rhino;
-            delete rhino['then']
-        })
-        } catch {
+        // try {
+        // await rhino3dm().then(rhino => {
+        //     this.rhinoModule = rhino;
+        //     delete rhino['then']
+        // })
+        // } catch {
 
-        }
+        // }
         
-        if (!this.rhinoModule) {
-            console.log('Failed to load rhino3dm wasm. Falling back to svgar-only mode.')
-            this.rhinoModule = {};
-        }     
+        // if (!this.rhinoModule) {
+        //     console.log('Failed to load rhino3dm wasm. Falling back to svgar-only mode.')
+        //     this.rhinoModule = {};
+        // }    
+        
+        this.rhinoModule = {}
 
         this.elements = new SvgarElementsContext(this.creamModule, this.rhinoModule);
 
